231eae851ec613aa8a53c6a890d9b71181d6f6ee
Benxamin <bertrand.design@gmail.com>
2014-10-06 17:06:48 -0600

    Adds component.js file

---
+++ component.js
@@ -0,0 +1,10 @@
+{
+  "name": "draggabilly",
+  "repo": "benxamin/draggabilly",
+  "description": "component.io version of Desandro's Draggabilly utility",
+  "version": "1.1.0",
+  "keywords": ["drag, draggabilly"],
+  "dependencies": {},
+  "scripts": ["draggabilly.js"],
+  "license": "MIT"
+}
